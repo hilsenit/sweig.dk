fb625cdb04fcb9617f54891d801cb4b8fc7b5c8d
peterhoejlundandersen <hilsenit@gmail.com>
2017-05-16 01:08:25 +0200

    just migrated and removed a migration for the searchfield, trying with controller now

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
 
 	def index
-		
-		
+	
+	@works = Work.all
+	if params[:search] # that's what the user is searching for
+		@works = Work.search(params[:search]).order(created_at: :desc)		
 	end
 end
--- app/models/work.rb
+++ app/models/work.rb
@@ -1,3 +1,7 @@
 class Work < ApplicationRecord
 	belongs_to :user
+
+	def self.search(searched_for)
+		where("name LIKE ?", "%#{searched_for}%")
+	end
 end
