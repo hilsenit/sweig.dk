539502336eb20465f90cb3cfb3157dc6107d0c30
peterhoejlundandersen <hilsenit@gmail.com>
2017-05-20 14:44:55 +0200

    added favicon

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
 
 	def index
-	
+	@users = User.all	
 	@works = Work.all
 		if params[:search] # that's what the user is searching for
 			@works = Work.search(params[:search]).order(created_at: :desc)		
