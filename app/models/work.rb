6215ee97349651b4d61d5984345be40f8466a106
peterhoejlundandersen <hilsenit@gmail.com>
2017-06-16 14:52:55 +0200

    søgning på værker virker

--- app/models/work.rb
+++ app/models/work.rb
@@ -5,8 +5,8 @@ class Work < ApplicationRecord
 	friendly_id :title, use: :slugged
 	enum status: { draft: 0, published: 1}
 	validates :title, :body, presence: true
-	
+
 	def self.search(searched_for)
-		where("title LIKE ? OR body LIKE ?", "%#{searched_for}%", "%#{searched_for}%")
+		where("title ILIKE ? OR body ILIKE ?", "%#{searched_for}%", "%#{searched_for}%")
 	end
 end
