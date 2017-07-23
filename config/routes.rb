562f826208a5efcd3f51e45fb01d4d9a0f526651
peterhoejlundandersen <hilsenit@gmail.com>
2017-07-24 01:22:50 +0200

    log-in error messages virker via flash

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
 
 
-	devise_for :users, controllers: { registrations: 'registrations' }
+	devise_for :users, controllers: { registrations: 'registrations', sessions: 'sessions' }
 
 	resources :users do
 		get "gemte-vaerker" => "users#saved_works", as: "saved_works"
