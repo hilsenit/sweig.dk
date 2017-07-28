86f324bcafe271162d18c8cc7349e3eda7d7fdf3
peterhoejlundandersen <hilsenit@gmail.com>
2017-07-29 00:11:39 +0200

    design

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,5 @@
 class Question < ApplicationRecord
-	has_many :votes
+	has_many :votes, dependent: :destroy
 	validates_presence_of :headline, message: "Overskriften skal udfyldes."
 	validates_presence_of :text, message: "Teksten skal udfyldes."
 
