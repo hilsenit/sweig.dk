f981cb2a6a334db8b59ce40551e48d43b394b177
peterhoejlundandersen <hilsenit@gmail.com>
2017-09-09 17:04:33 +0200

    integrated image in emails with https://stackoverflow.com/questions/4918414/what-is-the-right-way-to-embed-image-into-email-using-rails

--- app/mailers/bruger_mailer.rb
+++ app/mailers/bruger_mailer.rb
@@ -3,6 +3,8 @@ class BrugerMailer < Devise::Mailer
 	include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
 	default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
 	def confirmation_instructions(record, token, opts={})
+		attachments.inline["email-haeder.png"] = File.read("#{Rails.root}/app/assets/images/email-header.png")
+		attachments.inline["email-haeder-omvendt.png"] = File.read("#{Rails.root}/app/assets/images/email-header-omvendt.png")
 	  headers["Custom-header"] = "Aktiver din bruger"
 	  @username = record.username
 	  opts[:from] = 'kontakt@sweig.dk'
@@ -10,6 +12,8 @@ class BrugerMailer < Devise::Mailer
 	  super	
 	end
 	def reset_password_instructions(record, token, opts={})
+		attachments.inline["email-haeder.png"] = File.read("#{Rails.root}/app/assets/images/email-header.png")
+		attachments.inline["email-haeder-omvendt.png"] = File.read("#{Rails.root}/app/assets/images/email-header-omvendt.png")
 	  headers["Custom-header"] = "Nulstil dit kodeord"
 	  opts[:from] = 'kontakt@sweig.dk'
 	  opts[:reply_to] = 'kontakt@sweig.dk'
@@ -17,6 +21,8 @@ class BrugerMailer < Devise::Mailer
 	  super	
 	end
 	def unlock_instructions(record, token, opts={})
+		attachments.inline["email-haeder.png"] = File.read("#{Rails.root}/app/assets/images/email-header.png")
+		attachments.inline["email-haeder-omvendt.png"] = File.read("#{Rails.root}/app/assets/images/email-header-omvendt.png")
 	  headers["Custom-header"] = "Lås din bruger op"
 	  opts[:from] = 'kontakt@sweig.dk'
 	  opts[:reply_to] = 'kontakt@sweig.dk'
