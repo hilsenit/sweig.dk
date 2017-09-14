e4d2bd6397e54731c0061692f309c67f1934465e
peterhoejlundandersen <hilsenit@gmail.com>
2017-09-14 10:11:50 +0200

    tried to write test, didn't work

--- test/controllers/registrations_controller_test.rb
+++ test/controllers/registrations_controller_test.rb
@@ -1,7 +1,4 @@
 require 'test_helper'
 
 class RegistrationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  # For Devise >= 4.1.1
+    include Devise::Test::ControllerHelpers
+    # Use the following instead if you are on Devise <= 4.1.0
+    # include Devise::TestHelpers
+
+    def setup
+      @request.env["devise.mapping"] = Devise.mappings[:user]
+      sign_in users(:one)
+      binding.pry
+    end
 end
