3acd1c131d4497ad8cac7412258443d23e906886
peterhoejlundandersen <hilsenit@gmail.com>
2017-09-15 12:16:50 +0200

    change email seup and other stuff

--- config/initializers/email_setup.rb
+++ config/initializers/email_setup.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
 	ActionMailer::Base.smtp_settings = {
 	  address:              'smtp.sendgrid.net',
 	  port:                 587,
-	  domain:               'heroku.com',
+	  domain:               'sweig.dk',
 	  user_name:            ENV["SENDGRID_USERNAME"],
 	  password:             ENV["SENDGRID_PASSWORD"],
 	  authentication:       'plain',
