820fa0f04622cafa4cfd221bb06d64ebfbb73f20
peterhoejlundandersen <hilsenit@gmail.com>
2017-09-20 20:12:50 +0200

    wrote test checking if save_work biblo message was shown on users biblo_page

--- spec/requests/profile_spec.rb
+++ spec/requests/profile_spec.rb
@@ -2,10 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "UserProfile" do 
 	let(:user) {create(:user, :works)}
+	let(:active_user) {create(:user, :works)}
 
 	it "should not be possible for another user to visit the user-profile pages" do
-		user_visiting = create(:user) # The user trying to visit 
-		log_user_in user_visiting
+		active_user = create(:user) # The user trying to visit 
+		log_user_in active_user
 		%w(user_biblo user_saved_works user_my_works new_user_work).each do |path|
 			visit eval("#{path}_path(user.friendly_id)") 
 			expect(page).to have_current_path(user_path(user.friendly_id))
@@ -14,6 +15,20 @@ RSpec.describe "UserProfile" do
 		visit edit_user_work_path(user.friendly_id, user.works.first.friendly_id) # Opret værk
 		expect(page).to have_current_path(user_path(user.friendly_id))
 		expect(page).to have_content("Du kan ikke besøge")
+	end
+
+	it "biblo should be updated with save-message when saving work" do
+		log_user_in active_user
+		first_work = user.works.first
+		visit user_work_path(user.friendly_id, first_work.friendly_id)
+		click_link "GEM"
+		within(".show-navbar") do
+			expect(page).to have_content("'#{first_work.title}' er nu gemt")
+		end
+		visit user_biblo_path(active_user.friendly_id)
+		within(".biblo-wrapper") do
+			expect(page).to have_content("Du har gemt #{first_work.title}")
+		end
 	end
 
 
