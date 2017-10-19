cd649502321d1e3ca69287eacdaf07ee453fd64f
peterhoejlundandersen <hilsenit@gmail.com>
2017-10-19 20:33:22 +0200

    test is working again after installing DataBaseCleaner

--- spec/features/saved_work_on_off_spec.rb
+++ spec/features/saved_work_on_off_spec.rb
@@ -1,22 +1,16 @@
 require 'rails_helper'
 
 RSpec.feature 'ShowSavedWorksOnOff' do
-  before(:each) do
-    binding.pry
-    @user = create(:user)
-    @another_user = create(:user, :works)
-    @saved_work = create(:saved_work, user_id: @user.id, work_id: @another_user.works.last.id)
-  end
 
-# let(:user) {create(:user)}
-# let(:another_user) {create(:user, :works)}
-# let(:saved_work) { create(:saved_work, user_id: user.id, work_id: another_user.works.last.id) }
+let(:user) {create(:user)}
+let(:another_user) {create(:user, :works)}
+let(:saved_work) { create(:saved_work, user_id: user.id, work_id: another_user.works.last.id) }
 
   scenario "user should have the ability to hide saved works for other users" do
-    log_user_in @user
-    visit user_saved_works_path(@user.friendly_id)
+    log_user_in user
+    visit user_saved_works_path(user.friendly_id)
     within(".saved-works-wrapper") do
-      expect(page).to have_content(@another_user.works.last.title)
+      expect(page).to have_content(another_user.works.last.title)
     end
   end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -19,8 +19,47 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
-  # I've added THIS!!!
 
+  config.before(:suite) do
+    if config.use_transactional_fixtures?
+      raise(<<-MSG)
+        Delete line `config.use_transactional_fixtures = true` from rails_helper.rb
+        (or set it to false) to prevent uncommitted transactions being used in
+        JavaScript-dependent specs.
+
+        During testing, the app-under-test that the browser driver connects to
+        uses a different database connection to the database connection used by
+        the spec. The app's database connection would not be able to access
+        uncommitted transaction data setup over the spec's database connection.
+      MSG
+    end
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type: :feature) do
+    # :rack_test driver's Rack app under test shares database connection
+    # with the specs, so continue to use transaction strategy for speed.
+    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
+
+    if !driver_shares_db_connection_with_specs
+      # Driver is probably for an external browser with an app
+      # under test that does *not* share a database connection with the
+      # specs, so use truncation strategy.
+      DatabaseCleaner.strategy = :truncation
+    end
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
 
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
@@ -49,8 +88,8 @@ RSpec.configure do |config|
   # triggering implicit auto-inclusion in groups with matching metadata.
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-# The settings below are suggested to provide a good initial experience
-# with RSpec, but feel free to customize to your heart's content.
+  # The settings below are suggested to provide a good initial experience
+  # with RSpec, but feel free to customize to your heart's content.
 =begin
   # This allows you to limit a spec run to individual examples or groups
   # you care about by tagging them with `:focus` metadata. When nothing
