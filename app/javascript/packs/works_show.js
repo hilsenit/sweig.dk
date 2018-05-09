cb6989527b3b7cd15853136fa1f46f51d6d1a8c8
peterhoejlundandersen <hilsenit@gmail.com>
2018-05-09 14:14:53 +0200

    angular

--- app/javascript/packs/works_show.js
+++ app/javascript/packs/works_show.js
@@ -1,2 +1,1 @@
-
 require('../works_show');
--- app/javascript/works_show/app/app.component.ts
+++ app/javascript/works_show/app/app.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'works-show',
   template: `<h1>Hello {{name}}</h1>`
+
 })
 export class AppComponent {
-  name = 'HANDSOME';
+  name: String = 'HANDSOME';
 }
